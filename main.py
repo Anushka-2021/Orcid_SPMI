205e29cab2d380b7a39764ab8a67548329dcb3b5
Anushka-2021 <annamilk2021@mail.ru>
2022-07-11 19:07:13 +0300

    Now it shows all the table on website

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify, render_template
 import requests, sqlite3, sqlalchemy, json
 #from flask_restful import Api, Resourse, reqparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 app = Flask(__name__)
 
 host = 'https://pub.orcid.org/v3.0/'
@@ -19,24 +16,24 @@ def index():
         name_try = req.json()['name']['given-names']['value']
         surname_try = req.json()['name']['family-name']['value']
         keywords_try = req.json()['keywords']['keyword']
-        cursor.execute("SELECT * FROM orcid5 WHERE orcid_id=?", (orcid_id,))
-        rows = cursor.fetchone()
-        print(rows)
+        cursor.execute("SELECT * FROM orcid6 WHERE orcid_id=?", (orcid_id,))
+        row = cursor.fetchone()
+        cursor.execute("SELECT * FROM orcid6")
+        rows = cursor.fetchall()
 
-        if cursor.fetchone() is None: #orcid3.query(orcid3).filter_by(orcid_id=orcid_id).first():
-            cursor.execute("INSERT OR REPLACE INTO orcid5(name, surname, orcid_id) VALUES (?, ?, ?)", (name_try, surname_try, orcid_id))
-            conn.commit()
-    #    rows = cursor.execute("SELECT * FROM orcid2")#fetchall()
-    #    print(rows)
-    #    print(rows['name'])
+        # if cursor.fetchone() is None: #orcid3.query(orcid3).filter_by(orcid_id=orcid_id).first():
+        #     cursor.execute("INSERT OR REPLACE INTO orcid5(name, surname, orcid_id) VALUES (?, ?, ?)", (name_try, surname_try, orcid_id))
+        #     conn.commit()
 
-
-        print("orcid5:")
-        for value in cursor.execute("SELECT rowid, * FROM orcid5"):
-            print(value[0], value[1], value[2], value[3])
+        print("orcid6:")
+        f = 1
+        for value in cursor.execute("SELECT * FROM orcid6"):
+            print(f, value[0], value[1], value[2], value[3])
+            f = f + 1
         print()
 
-        return render_template("2.html", table=rows)
+        return render_template("3.html", table=rows)
+        #return render_template("2.html", table=row)
     else:
         return render_template("1.html")
 #   req = requests.get(host + '/0000-0002-3707-6813/person', headers = {'Accept': 'application/vnd.orcid+json'})
@@ -48,10 +45,11 @@ def about_person(orcid_id):
     return req.json['name']['given-names']['value']#, req.json['name']['family-names']['value']
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
     count = 0
-    cursor.execute("""CREATE TABLE IF NOT EXISTS orcid5
-                (name text, surname text, orcid_id text UNIQUE)
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS orcid6
+                (name text, surname text, orcid_id text UNIQUE, k_words text)
                 """)
     conn.commit()
 
@@ -67,12 +65,25 @@ if __name__ == "__main__":
             count = count + 1
             name = person_req.json()['name']['given-names']['value']
             surname = person_req.json()['name']['family-name']['value']
+            kwords = []
+            kwords_str = ''
+            keywords_req = person_req.json()['keywords']['keyword']
+            print("!?!")
+            print(len(keywords_req))
+            print("!?!")
+          #  kwords_str = person_req.json()['keywords']['keyword']['content']
+            for i in keywords_req:
+                # if i == len(keywords_req):
+                #     kwords_str = kwords_str + i['content']
+                # else:
+                    kwords_str = kwords_str + i['content'] + '; '
             if cursor.fetchone() is None:
-                cursor.execute("INSERT OR REPLACE INTO orcid5(name, surname, orcid_id) VALUES (?, ?, ?)", (name, surname, orcid_id))
+                cursor.execute("INSERT OR REPLACE INTO orcid6(name, surname, orcid_id, k_words) VALUES (?, ?, ?, ?)", (name, surname, orcid_id, kwords_str))
                 conn.commit()
     print("!!!")
     print(count)
     print("!!!")
+    app.run(debug=True)
 
     # for i in mining_search_res:
     #      print(i['orcid-identifiers']['path'])
