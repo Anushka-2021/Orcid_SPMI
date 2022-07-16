cb1b064bc8e300f4bbb576dd5e82f4c92c656abf
Anushka-2021 <annamilk2021@mail.ru>
2022-07-16 20:12:58 +0300

    Almost final

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, render_template
-import requests, sqlite3, datetime, eel, webbrowser
+import requests, sqlite3, datetime
 
 app = Flask(__name__)
-eel.init("Praktika_2022")
 
 host = 'https://pub.orcid.org/v3.0/'
 conn = sqlite3.connect("orcid1.db", check_same_thread=False)
@@ -10,92 +9,50 @@ cursor = conn.cursor()
 
 count = 0
 global temp
-resp = []#?
-
+resp = []#I really don't remember if we're need in it...
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        orcid_id = request.form.get('id')
-        cursor.execute("SELECT * FROM orcid8 WHERE orcid_id=?", (orcid_id,))
-        row = cursor.fetchone()
-    #     print(cursor.execute("SELECT * FROM orcid8"))
-    #     c = 0
-    #     for i in range(19):
-    #         print(c)
-    #         c = c + 1
-    #         resp.append(cursor.fetchmany(size=25))
-    #     rows = resp
-    #     print(resp)
-    #     print("orcid8:")
-    #     f = 1
-    #     for value in cursor.execute("SELECT * FROM orcid8"):
-    #         print(f, value[0], value[1], value[2], value[3], value[4], value[5], value[6], value[7], value[8], value[9])
-    #         f = f + 1
-    #     print()
-    #     print(rows)
-        return render_template("7.html", table=row)
+        orcid_id = request.form.get('orcid_id')
+        doi = request.form.get('doi')
+        if orcid_id != None:
+            cursor.execute("SELECT * FROM orcid10 WHERE orcid_id=?", (orcid_id,))
+            row = cursor.fetchall()
+            return render_template("7.html", table=row)
+        if doi != None:
+            cursor.execute("SELECT * FROM orcid10 WHERE doi=?", (doi,))
+            row = cursor.fetchall()
+            return render_template("7.html", table=row)
     else:
         return render_template("8.html")
 
-# @app.route('/big_base', methods=['GET'])
-# def bb():
-#     mining_search_res = requests.get(host + 'search/?q=affiliation-org-name:"Saint+Petersburg+Mining+University"', headers = {'Accept': 'application/vnd.orcid+json'}).json()['result']
-#     for i in mining_search_res:#Внос данных в таблицу БД
-#         orcid_id = i['orcid-identifier']['path']
-#         mass = []
-#         person_req = requests.get(host + orcid_id + '/person', headers = {'Accept': 'application/vnd.orcid+json'})
-#         works_req = requests.get(host + orcid_id + '/works', headers = {'Accept': 'application/vnd.orcid+json'}).json()['group']
-#         if person_req.json()['name'] is None or person_req.json()['name']['given-names'] is None or person_req.json()['name']['family-name'] is None:# person_req.json():
-#             name = 'None'
-#             surname = 'None'
-#         else:
-#             name = person_req.json()['name']['given-names']['value']
-#             surname = person_req.json()['name']['family-name']['value']
-#
-#         kwords_str = ''
-#         keywords_req = person_req.json()['keywords']['keyword']
-#         for i in keywords_req:
-#             kwords_str = kwords_str + i['content'] + '; '
-#
-#         mass.append({"name": name, "surname": surname, "keywords": kwords_str})
-#         print(mass)
-#         # works_str = ''
-#         # for i in works_req:
-#         #     a = i['external-ids']['external-id']
-#         #     for j in a:
-#         #         if j['external-id-type'] == "doi":
-#         #             works_str = works_str + j['external-id-value'] + '; '
-#     return render_template("5.html", table=mass)
-
-
 @app.route('/table', methods=['GET', 'POST'])
 def table():
-    cursor.execute("SELECT * FROM orcid8")
-    for i in range(30):
-        resp.append(cursor.fetchmany(size=25))
+    cursor.execute("SELECT * FROM orcid10")
+    resp.append(cursor.fetchall())
+    # for i in range(30):
+    #     resp.append(cursor.fetchmany(size=25))
     return render_template("6.html", table=resp)
 
 @app.route('/page<int:page_number>', methods=['GET', 'POST'])#общее для страниц списка
 def page(page_number):
     resp = []
-    cursor.execute("SELECT * FROM orcid8")
+    cursor.execute("SELECT * FROM orcid10")
     for i in range(page_number-1):
         cursor.fetchmany(size=25)
     for i in range(1):
         resp.append(cursor.fetchmany(size=25))
     return render_template("6.html", table=resp)
 
-@app.route('/page<int:page_number>/<swap>', methods=['GET', 'POST'])#общее для страниц списка
-def list(page_number, swap):
-    if swap == 'next':
-        webbrowser.open('http://127.0.0.1:5000/page'+str(page_number+1))
-    if swap == 'prev':
-        webbrowser.open('http://127.0.0.1:5000/page'+str(page_number-1))
+# @app.route('/page<int:page_number>/<swap>', methods=['GET', 'POST'])#общее для страниц списка
+# def list(page_number, swap):
+#     if swap == 'next':
+#         webbrowser.open('http://127.0.0.1:5000/page'+str(page_number+1))
+#     if swap == 'prev':
+#         webbrowser.open('http://127.0.0.1:5000/page'+str(page_number-1))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
 
     now = datetime.datetime.now()#time
     time = now.strftime("%H:%M:%S")
@@ -105,8 +62,18 @@ if __name__ == "__main__":
     if current_hour=='03' and current_min=='0' and current_sec=='0':
         print("DATABASE WILL BE UPDATED")#вот в этой штуке должно быть всё что обновляет ДБ (наверное)
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS orcid8
-                (doi, wosid, summary_str, orcid_id, name, surname, other_names, country, external_ids, k_words)
+    # cursor.execute("""CREATE TABLE IF NOT EXISTS orcid8
+    #             (doi, wosid, summary_str, orcid_id, name, surname, other_names, country, external_ids, k_words)
+    #             """)
+    # conn.commit()
+
+    # cursor.execute("""CREATE TABLE IF NOT EXISTS orcid9
+    #             (doi UNIQUE, wosid, summary_str, orcid_id, name, surname, other_names, country, external_ids, k_words)
+    #             """)
+    # conn.commit()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS orcid10
+                (doi UNIQUE, wosid UNIQUE, eid UNIQUE, summary_str, orcid_id, name, surname, other_names, country, external_ids, k_words)
                 """)
     conn.commit()
 
@@ -153,24 +120,44 @@ if __name__ == "__main__":
             ids = i['external-ids']['external-id']
             wos = None
             doi = None
+            eid = None
             for j in ids:
-                if j['external-id-type'] == "wosuid":
+                if j['external-id-type'] == 'wosuid':
                     wos = j['external-id-value']
-                if j['external-id-type'] == "doi":
-                    wos = j['external-id-value']
+                if j['external-id-type'] == 'doi':
+                    doi = j['external-id-value']
+                if j['external-id-type'] == 'eid':
+                    eid = j['external-id-value']
+
+
 
             w_summary = i['work-summary']
             summary_str = ''
             for j in w_summary:
 
-                if doi == None and j['external-ids'] != None and j['external-ids']['external-id'] != None:
+                if j['external-ids'] != None and j['external-ids']['external-id'] != None:
                     for k in j['external-ids']['external-id']:
-                        if k['external-id-type'] == 'doi' and k['external-id-value'] != ('none' or None):
-                            doi = k['external-id-value']
-                if wos == None and j['external-ids'] != None and j['external-ids']['external-id'] != None:
-                    for k in j['external-ids']['external-id']:
-                        if k['external-id-type'] == 'wosuid' and k['external-id-value'] != ('none' or None):
-                            wos = k['external-id-value']
+                        if k['external-id-type'] == 'doi':
+                            if doi == None:
+                                doi = k['external-id-value']
+                        if k['external-id-type'] == 'wosuid':
+                            if wos == None:
+                                wos = k['external-id-value']
+                        if k['external-id-type'] == 'eid':
+                            if eid == None:
+                                eid = k['external-id-value']
+                print()
+                print(doi, '    ', wos, '   ', eid)
+
+
+                # if doi == None and j['external-ids']['external-id'] != None:
+                #     for k in j['external-ids']['external-id']:
+                #         if k['external-id-type'] == 'doi' and k['external-id-value'] != None:
+                #             doi = k['external-id-value']
+                # if wos == None and j['external-ids']['external-id'] != None:
+                #     for k in j['external-ids']['external-id']:
+                #         if k['external-id-type'] == 'wosuid' and k['external-id-value'] != None:
+                #             wos = k['external-id-value']
 
                 put_code = j['put-code']
                 source_name = j['source']['source-name']['value']
@@ -180,26 +167,28 @@ if __name__ == "__main__":
                 work_publication_date = ''
                 if j['publication-date'] != None:
                     if j['publication-date']['day'] != 'null' and j['publication-date']['day'] != None:
-                        work_publication_date += j['publication-date']['day']['value']
+                        work_publication_date += j['publication-date']['day']['value'] + '.'
                     if j['publication-date']['month'] !='null' and j['publication-date']['month'] != None:
                         work_publication_date += '.' + j['publication-date']['month']['value']
                     else:
-                        work_publication_date += '.xx'
+                        work_publication_date += 'xx.'
                     if j['publication-date']['year'] != 'null' and j['publication-date']['year'] != None:
                         work_publication_date += '.' + j['publication-date']['year']['value']
                     else:
-                        work_publication_date += '.xxxx'
+                        work_publication_date += 'xxxx'
                 else:
                     work_publication_date = 'None'
 
                 if j['journal-title'] != None:
                     journal_title = j['journal-title']['value']
-                summary_str += str(put_code) + ', ' + source_name + ', ' + work_title + ', ' + work_type + ', ' + work_publication_date + ', ' + journal_title + '; '
+                summary_str += 'source name: ' + str(source_name) + ', title: ' + str(work_title) + ', type: ' + str(work_type) + ', work publicaion date:' + str(work_publication_date) + ', journal title: ' + str(journal_title) + '; '
             print(doi, wos, summary_str, orcid_id, name, surname, other_names, country, external_ids, kwords_str)
+
             if cursor.fetchone() is None:
-                cursor.execute("INSERT OR REPLACE INTO orcid8(doi, wosid, summary_str, orcid_id, name, surname, other_names, country, external_ids, k_words) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (doi, wos, summary_str, orcid_id, name, surname, other_names, country, external_ids, kwords_str))
+                cursor.execute("INSERT OR REPLACE INTO orcid10(doi, wosid, eid, summary_str, orcid_id, name, surname, other_names, country, external_ids, k_words) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (doi, wos, eid, summary_str, orcid_id, name, surname, other_names, country, external_ids, kwords_str))
                 conn.commit()
 
+    app.run(debug=True)
     #Всё ниже для маленькой таблицы
     # cursor.execute("""CREATE TABLE IF NOT EXISTS orcid7
     #             (name text, surname text, orcid_id text UNIQUE, k_words text, works text)
@@ -238,4 +227,3 @@ if __name__ == "__main__":
     #     if cursor.fetchone() is None:
     #         cursor.execute("INSERT OR REPLACE INTO orcid7(name, surname, orcid_id, k_words, works) VALUES (?, ?, ?, ?, ?)", (name, surname, orcid_id, kwords_str, works_str))
     #         conn.commit()
-
